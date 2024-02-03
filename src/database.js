62b0a91e656f09960ac535a880b73a2921616dea
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-02-03 23:00:20 +0530

    commit

--- src/database.js
+++ src/database.js
@@ -1,1 +1,13 @@
 const mongoose = require("mongoose");
+
+mongoose.connect("").then(() =>{
+    console.log('Connected to MongoDB');
+}).catch((error) =>
+console.log(error));
+
+Schema = mongoose.Schema({
+    Task: {type : String , required: true},
+
+});
+
+MyModel = mongoose.model("MyApp",Schema);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const path = require("path");
 app = express();
 port = 8080;
 
 app.listen(port ,()=>{console.log( `My Server is running on ${port}`)});
 
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(path.join(__dirname,"../public")));
+
+app.get("/"  , (req,res) => {
+    res.render('index');
+    });
+
+// CRUD Operations
